137b2b45d4fbd0999386548a0887a0729ba5c681
sakuma.atsushi <sakuma-a1@budong.co.jp>
2024-12-27 15:34:08 +0900

    Revert "using single quote"
    This reverts commit b0c48c7c2d7c4b59880576995d9072437a9ecfb5.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   base: '/'
+=======
+  base: "/eng-game-project/",
+>>>>>>> parent of b0c48c7 (using single quote)
 });
