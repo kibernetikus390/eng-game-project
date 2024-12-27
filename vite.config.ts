58f0275c328c3d53b91fff1d6dffbac933994e70
sakuma.atsushi <sakuma-a1@budong.co.jp>
2024-12-27 15:44:22 +0900

    test publish /docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/'
-=======
   base: "/eng-game-project/",
->>>>>>> parent of b0c48c7 (using single quote)
 });
