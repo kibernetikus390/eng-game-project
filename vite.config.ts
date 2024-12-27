ab97085a6116d14cf8a257a5854ff11a2823deb0
sakuma.atsushi <sakuma-a1@budong.co.jp>
2024-12-27 15:49:51 +0900

    viteconfig base:"/"

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/eng-game-project/",
+  base: "/",
+  // base: "/eng-game-project/",
 });
